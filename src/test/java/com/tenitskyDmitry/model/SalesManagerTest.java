4d8adf44caefb54f72a8986ebaf8cd0a31c5a7ca
redares <lodroft@gmail.com>
2022-01-10 03:33:27 +0300

    Added tests files.

---
+++ src/test/java/com/tenitskyDmitry/model/SalesManagerTest.java
@@ -0,0 +1,12 @@
+package com.tenitskyDmitry.model;
+
+import org.junit.Test;
+
+import static org.junit.Assert.*;
+
+public class SalesManagerTest {
+
+    @Test
+    public void getSalary() {
+    }
+}
